334fa2eca63bfd556b4a0593eb528095df833007
Vivek Narayanan <155041249+Vivek11N@users.noreply.github.com>
2024-03-04 17:41:15 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit",function(event){
+    document.getElementById("name") .value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+});
